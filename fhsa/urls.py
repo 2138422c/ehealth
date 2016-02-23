414ba83093980adaa7d250c19771728eb98695f9
2138422c <2138422C@student.gla.ac.uk>
2016-02-23 12:24:31 +0000

    urls set to non-depreciated version

--- fhsa/urls.py
+++ fhsa/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
 from . import views
 
-urlpatterns = patterns ('', url(r'^$', views.index, name='index'))
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    ]
