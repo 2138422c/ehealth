ec6591b694d3e8968c1bc50b4dfac7812d49cfc7
2138422c <2138422C@student.gla.ac.uk>
2016-03-02 12:47:29 +0000

    Feautures add to sensitivity.py

--- sensitivity.py
+++ sensitivity.py
@@ -6,7 +6,6 @@ __author__ = 'alistaircross'
     This sensitivity will be a dictionary:
         overall sensitvity
         sentiment
-        length
         readability
 """
 from textblob import TextBlob
@@ -14,6 +13,19 @@ from textstat.textstat import textstat
 
 text = "The quick brown fox jumped over the lazy dog." # Replace with actual text when available
 
-blob = TextBlob(text)
-sentimentality = blob.subjectivity * 100
-readability = textstat.automated_readability_index(text) * 10
+def  get_sensitivity_rating(text):
+    blob = TextBlob(text)
+    sentimentality = blob.subjectivity * 100
+    readability = textstat.flesch_reading_ease(text)
+    sensitivity = (sentimentality + readability)/2
+    print sensitivity
+    if sensitivity < 50:
+        print "This article may not handle the subject in a sensitive manner. \n\n"
+    print sentimentality
+    if sentimentality < 50:
+        print "This article may loaded with subjectivity.\n\n"
+    print readability
+    if readability < 50:
+        print "This article contains content that is difficult to understand\n\n"
+
+get_sensitivity_rating(text)
