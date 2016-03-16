e6cb62d83da121e5f26ba8c69da890ebfcaa05a6
Kieran McCool <kieranmccool332@gmail.com>
2016-03-16 15:35:07 +0000

    see last commit

--- searchWrap.py
+++ searchWrap.py
@@ -13,7 +13,11 @@ def queryFormat(s):
 def stripHtml(s):
 	return re.sub('<[^>]*>', '', s)
 
-def medlineSearch(term):
+def medlineSearch(term, debug=False):
+
+	if debug:
+		return testML(term)
+
 	base = "https://wsearch.nlm.nih.gov/ws/query"
 	base = urlBuilder(base, { "db":"healthTopics", "term":queryFormat(term)})
 
@@ -31,7 +35,11 @@ def medlineSearch(term):
 
 	return out
 
-def bingSearch(term):
+def bingSearch(term, debug=False):
+
+	if debug:
+		return testBing(term)
+
 	credentialBing = 'Basic ' + (':%s' % bingKey).encode('base64')[:-1] # the "-1" is to remove the trailing "\n" which encode adds
 
 	top = 20
@@ -49,13 +57,16 @@ def bingSearch(term):
 	return results
 
 
-def hfSearch(term, age, gender, who="someone", pregnant=0):
+def hfSearch(term, age, gender, who="someone", pregnant=0, debug=False):
 	def hfSillyFormatting(s):
 		l = s.split(" ")
 		out = "%%22%s" % l[0]
 		for i in range(1, len(l)):
 			out += "%%20%s" % (l[i])
 		return out + "%22"
+
+	if debug:
+		return testHF(term, age, gender, who=who, pregnant=pregnant)
 
 	base = "http://healthfinder.gov/developer/Search.json"
 	base = urlBuilder(base, { "keyword":hfSillyFormatting(term), "gender":gender,
@@ -77,23 +88,24 @@ def urlBuilder(baseUrl, dict):
 	return baseUrl + out
 
 #print urlBuilder("www.kieranisgod.com", { "query":"toast is good", "user":"Kieran McCool" })
-def testBing():
-	d =  bingSearch("Toast and beans") # I was eating those at the time of writing.
+def testBing(query):
+	d =  bingSearch(query) # I was eating those at the time of writing.
 	f = open("testBing.txt", "w+")
 	for r in d:
 		f.write("\n%s\n" % r)
+	return d
 
-def testML():
-	d = medlineSearch("chronic diarrhea")
+def testML(query):
+	d = medlineSearch(query)
 	f = open("testMedLine.txt", "w+")
 	for i in d:
 		f.write(str(i) + "\n\n")
+	return d
 
-def testHF():
+def testHF(query, age, gender, who="someone", pregnant=0):
 	f = open("testHealthFinder.txt", "w+")
-	d = hfSearch("Pain", 19, "male")
+	d = hfSearch(query, age, gender, who=who, pregnant=pregnant)
 	f.write(str(d))
-	for r in d:
-		f.write(str(r) + "\n\n" )
+	return d
 
 #testBing(); testML(); testHF()
